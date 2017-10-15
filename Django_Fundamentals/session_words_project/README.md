dd90d80f76082e8acfd24c5666781351541e75fd
Marina Ikeno <mikeno61@gmail.com>
2017-10-15 16:44:35 -0700

    added session_words assignment

---
+++ Django_Fundamentals/session_words_project/README.md
@@ -0,0 +1,11 @@
+# Session Words Assignment
+The purpose of this assignment was to use forms and sessions in Django, specifically to get familiar with use of radio buttons and checkboxes. 
+<br>
+The requirements were: 
+* Create a new app called 'session_words'
+* Allow the user to add any new word to the session
+* The user should be able to specify the color to display the word and whether the word is in a bigger font
+* The user should be able to clear the session
+* Show the words stored in the session on the right
+
+I have also added error messages in case the user doesn't input a word or select a color. 
---
+++ Django_Fundamentals/session_words_project/apps/session_words_app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect, HttpResponse
+from time import gmtime, strftime
+from django.contrib import messages
+from django.utils.datastructures import MultiValueDictKeyError
+
+# Create your views here.
+def index(request):
+	return render(request,'session_words_app/index.html')
+
+def add_word(request):
+	word=request.POST['new_word']
+	if not len(word):
+		messages.warning(request, 'Please enter a word')
+		return redirect('/')
+
+	if 'color' in request.POST:
+		color=request.POST['color']	
+	else:
+		color = False
+		messages.warning(request, 'Please select a color')
+		return redirect('/')
+
+	if 'big' in request.POST:
+		big = request.POST['big']
+	else:
+		big = False
+
+	request.session['time'] = strftime("%I:%M:%S%p, %B %dth %Y", gmtime())
+
+	try:
+		request.session['word_list'].append({'word':word, 'w_color':color, 'big':big, 'time': request.session['time']})
+	except: 
+		request.session['word_list']=[{'word':word, 'w_color':color, 'big':big, 'time': request.session['time']}]
+	return redirect('/')
+
+def clear(request):
+	request.session['word_list']=[]
+	return redirect('/')
