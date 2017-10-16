4c0ae141cbf0e1e10b95228b905721fdb65769c2
Marina Ikeno <mikeno61@gmail.com>
2017-10-16 14:58:28 -0700

    edited session

--- Django_Fundamentals/session_words_project/apps/session_words_app/views.py
+++ Django_Fundamentals/session_words_project/apps/session_words_app/views.py
@@ -5,6 +5,8 @@ from django.utils.datastructures import MultiValueDictKeyError
 
 # Create your views here.
 def index(request):
+	if 'word_list' not in request.session:
+		request.session['word_list']=[]
 	return render(request,'session_words_app/index.html')
 
 def add_word(request):
@@ -26,11 +28,7 @@ def add_word(request):
 		big = False
 
 	request.session['time'] = strftime("%I:%M:%S%p, %B %dth %Y", gmtime())
-
-	try:
-		request.session['word_list'].append({'word':word, 'w_color':color, 'big':big, 'time': request.session['time']})
-	except: 
-		request.session['word_list']=[{'word':word, 'w_color':color, 'big':big, 'time': request.session['time']}]
+	request.session['word_list'].append({'word':word, 'w_color':color, 'big':big, 'time': request.session['time']})
 	return redirect('/')
 
 def clear(request):
